db5a57c7a46186fc48ad56c4eb09b7216876f71f
= <=>
2024-04-15 17:36:24 +0100

    added class4 js file

---
+++ class4.js
@@ -0,0 +1,49 @@
+/*scores = [70, 85, 61, 90, 54];
+console.log(scores);
+//pop function + removes the last element in an array
+lastscore = scores.pop();
+lastscore = (scores);
+scores.push(32);
+firstScore = scores.shift();
+console.log(scores);
+//console.log(lastscore);
+console.log(firstScore);*/
+
+// strings
+/*time = 9
+statement1 = "jude goes to work at" + time + "AM" ; // concatennation
+statement2 = "mike goes to work at + $(time) + AM" ; // interpolation
+console.log(statement1);
+console.log(statement2);
+
+statement3 = 'my car is "large" like an elephant'
+console.log(statement3);*/
+
+//logical operators
+let a = 3;
+let b = 6;
+let c = 8;
+/*console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log( a>= b);
+console.log(a == b);
+console.log(a != b);
+console.log(a == "3");
+console.log(a === "3");
+console.log(a == 3);
+console.log(a > 1 && a < c);
+console.log(a > b && a < c);
+console.log(a > b || a < c);*/
+
+// if statement
+if (a > b) {
+    console.log("person A is taller");
+} 
+
+else if (a == b) {
+    console.log("person A is equal in height");
+} 
+else {
+    console.log("person A is shorter");
+}  
