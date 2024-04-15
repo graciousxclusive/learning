43211a430921438dea876c9a9080ef8e35891406
= <=>
2024-04-15 17:37:43 +0100

    added all files

---
+++ class5.js
@@ -0,0 +1,10 @@
+function sayHi() {
+    console.log("hello there")
+   function sayHi() 
+   }
+   sayHi()
+   
+   function sayHi2(name){
+    console.log("hello there," + name);
+   }
+   sayHi2(olamide)
